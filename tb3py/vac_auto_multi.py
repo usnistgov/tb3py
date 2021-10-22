6d94d126a3f0360eed00142691a954a05c6f9ec6
JARVIS-Unifies <writetokamal.1989@gmail.com>
2021-10-22 01:22:11 -0400

    Modified vacancy.

--- tb3py/vac_auto_multi.py
+++ tb3py/vac_auto_multi.py
@@ -13,16 +13,16 @@ dat = data("dft_3d")
 
 jids=['JVASP-910', 'JVASP-867', 'JVASP-922', 'JVASP-21195', 'JVASP-1002', 'JVASP-816', 'JVASP-972', 'JVASP-943', 'JVASP-852', 'JVASP-890', 'JVASP-922', 'JVASP-987', 'JVASP-14832', 'JVASP-14837', 'JVASP-14622', 'JVASP-14744', 'JVASP-14812', 'JVASP-14812', 'JVASP-14601', 'JVASP-14603', 'JVASP-14604', 'JVASP-14606', 'JVASP-14612', 'JVASP-14725', 'JVASP-88846', 'JVASP-7804', 'JVASP-858', 'JVASP-79561', 'JVASP-828', 'JVASP-25104', 'JVASP-25144', 'JVASP-919', 'JVASP-25273', 'JVASP-916', 'JVASP-1035', 'JVASP-25254', 'JVASP-25250', 'JVASP-25248', 'JVASP-25142', 'JVASP-949', 'JVASP-949', 'JVASP-25213', 'JVASP-937', 'JVASP-937', 'JVASP-25210', 'JVASP-25407', 'JVASP-25407', 'JVASP-1017', 'JVASP-1017', 'JVASP-946', 'JVASP-1020', 'JVASP-1050', 'JVASP-21197', 'JVASP-25388', 'JVASP-1014', 'JVASP-958', 'JVASP-25180', 'JVASP-25379', 'JVASP-25125', 'JVASP-25125', 'JVASP-870', 'JVASP-870', 'JVASP-993', 'JVASP-802', 'JVASP-21193', 'JVASP-901', 'JVASP-25117', 'JVASP-25117', 'JVASP-861', 'JVASP-931', 'JVASP-25114', 'JVASP-981', 'JVASP-834', 'JVASP-33718', 'JVASP-966', 'JVASP-966', 'JVASP-810', 'JVASP-810', 'JVASP-898', 'JVASP-25337', 'JVASP-825', 'JVASP-837', 'JVASP-1056', 'JVASP-969', 'JVASP-969', 'JVASP-961', 'JVASP-1011', 'JVASP-895', 'JVASP-934', 'JVASP-840', 'JVASP-984', 'JVASP-819', 'JVASP-25167', 'JVASP-1029', 'JVASP-1029', 'JVASP-996', 'JVASP-910', 'JVASP-963', 'JVASP-922', 'JVASP-1026', 'JVASP-95268', 'JVASP-102277', 'JVASP-102277']
 
-#jids=['JVASP-816'] #Al
-jids=['JVASP-867'] #Cu
 jids=['JVASP-14971']
 jids=['JVASP-32'] #Al2O3
 #chempot_maker1.py
+jids=['JVASP-816'] #Al
+jids=['JVASP-867'] #Cu
 chempot=loadjson("chempot.json")
 def get_mono_vac_energy(atoms=None, id=""):
     strts = Vacancy(atoms).generate_defects(
-        on_conventional_cell=True, enforce_c_size=5, extend=1
-        #on_conventional_cell=True, enforce_c_size=10, extend=0
+        on_conventional_cell=True, enforce_c_size=10, extend=0
+        #on_conventional_cell=False, enforce_c_size=8, extend=1
     )
     #print ('STRTS',len(strts))#,[j.to_dict['symbol'] for j in strts])
     perf_atoms = Atoms.from_dict(strts[0].to_dict()["atoms"])
@@ -49,13 +49,13 @@ def get_mono_vac_energy(atoms=None, id=""):
         #info_def = get_energy(strt,relax_atoms=True)
  
   
-        print(' Vacancy name',name, j.to_dict()["symbol"])
-        def_en = info_def["energy"] - epa * perf_atoms.num_atoms + chempot[j.to_dict()["symbol"]]
+        def_en = info_def["energy"] - epa * perf_atoms.num_atoms + chempot[j.to_dict()["symbol"]]['energy']
+        print(' Vacancy name',name, j.to_dict()["symbol"],def_en)
         info={}
         info['name']=name
         info['def_en']=def_en
-        info['epa']=def_en
-        info['perf_atoms']=info_perfect.to_dict()
+        info['energy']=info_def["energy"] 
+        info['perf_atoms']=info_perfect#.to_dict()
         info['defect_atoms']=strt.to_dict()
         mem.append(info)
 
