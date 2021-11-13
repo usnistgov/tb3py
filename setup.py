5c5fe69e2c2ead1725e254223564008dcd199fb8
knc6 <writetokamal.1989@gmail.com>
2021-11-13 16:40:07 -0500

    Fix main.

--- setup.py
+++ setup.py
@@ -34,11 +34,7 @@ def install(julia=None):
     print("install ", julia)
     # NOTE: For a new Github release:
     # rm ~/.julia/sysimages/sys_threebodytb.so
-    os.system(
-        julia
-        + " --eval "
-        + '"import Pkg; Pkg.add(ThreeBodyTB")"'
-    )
+    os.system(julia + " --eval " + '"import Pkg; Pkg.add(ThreeBodyTB")"')
     # os.system(julia + " --eval " + '"import Pkg; Pkg.add(\\"ThreeBodyTB\\")"')
     os.system(julia + " --eval " + '"import Pkg; Pkg.add(\\"PyCall\\")"')
     os.system(julia + " --eval " + '"import Pkg; Pkg.add(\\"Plots\\")"')
@@ -58,6 +54,10 @@ setup(
         "cython",
         "pytest",
     ],
+    extras_require={
+        "jarvis-tools": ["jarvis-tools"],
+        "julia": ["julia"],
+    },
     author="Kevin Garrity, Kamal Choudhary",
     author_email="kevin.garrity@nist.gov",
     description=(
@@ -105,54 +105,7 @@ hasjulia = (
 print("hasjulia: ", hasjulia)
 
 if not hasjulia:  # if the correct Julia version is not present
-    julia_cmd = os.path.join(
-        mpath, "tb3py", "julia", "julia-1.6.1", "bin", "julia"
-    )  # mpath+"/src/julia/julia-1.6.1/bin/julia" # path for julia
-    # julia_cmd = mpath+"/src/julia/julia-1.6.1/bin/julia" # path for julia
-    if not os.path.exists(julia_cmd):
-        print("Julia not present in path:", mpath, "...downloading...")
-        subfolder = os.path.join(mpath, "tb3py", "julia")
-        if not os.path.exists(subfolder):
-            os.makedirs(subfolder)
-        # os.system("mkdir "+mpath+"/src/julia") # create a subfodler for julia
-        os.chdir(subfolder)  # go to the subfolder
-        # os.chdir(mpath+"/src/julia") # go to the subfolder
-        ## download julia
-        juliafile = os.path.join(
-            mpath, "julia-1.6.1-linux-x86_64.tar.gz"
-        )  # julia file
-        # juliafile = os.path.join(mpath,"julia-1.6.1-linux-x86_64.tar.gz")  # julia file
-        # j_link = "https://julialang-s3.julialang.org/bin/linux/x64/1.6/"
-        j_link = (
-            "https://julialang-s3.julialang.org/bin/linux/x64/1.6/"
-            + "julia-1.6.1-linux-x86_64.tar.gz"
-        )
-        print("jlink", j_link)
-        import requests
-        import tarfile
-
-        r_julia = requests.get(j_link, stream=True).content
-        with open(juliafile, "wb") as jfile:
-            jfile.write(r_julia)
-        print("juliafile", juliafile)
-        try:
-            my_tar = tarfile.open(juliafile)
-            my_tar.extractall()
-            my_tar.close()
-        except Exception as exp:
-            print(exp)
-            cmd = "tar -xvzf " + juliafile
-            os.system(cmd)
-        os.remove(juliafile)
-        # os.system("wget https://julialang-s3.julialang.org/bin/linux/x64/1.6/"+juliafile)
-        # os.system("tar -xvf "+juliafile) # untar the file
-        # os.system("rm "+juliafile) # rm the file
-    else:
-        print("We already downloaded to ", julia_cmd)
-
-    # julia_cmd = mpath+"/src/julia/julia-1.6.1/bin/julia" # path for julia
-    os.chdir(mpath)  # go back
-
+    raise ValueError("conda install -c conda-forge julia")
 else:
     julia_cmd = "julia"
     print("Correct Julia version found in path")
@@ -179,10 +132,10 @@ print("sysimage", sysimage)
 if not os.path.isfile(sysimage):
     # import TB3.juliarun as juliarun
     install(julia_cmd)  # install Julia dependences
-julia_bin = os.path.join(
-    mpath, "tb3py", "julia", "julia-1.6.1", "bin"
-)  # mpath+"/src/julia/julia-1.6.1/bin/julia" # path for julia
-os.environ["PATH"] += os.pathsep + os.path.join(julia_bin)
+# julia_bin = os.path.join(
+#    mpath, "tb3py", "julia", "julia-1.6.1", "bin"
+# )  # mpath+"/src/julia/julia-1.6.1/bin/julia" # path for julia
+# os.environ["PATH"] += os.pathsep + os.path.join(julia_bin)
 """
 
 from julia.api import Julia
--- tb3py/main.py
+++ tb3py/main.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 from julia.api import Julia
 from jarvis.core.kpoints import Kpoints3D as Kpoints
+
 # from jarvis.core.kpoints import generate_kgrid
 
 import matplotlib.pyplot as plt
@@ -22,21 +23,7 @@ plt.switch_backend("agg")
 sysimage = os.path.join(
     os.environ["HOME"], ".julia", "sysimages", "sys_threebodytb.so"
 )
-julia_cmd = mpath = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    "julia",
-    "julia-1.6.1",
-    "bin",
-    "julia",
-)
-julia_bin = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    "tb3py",
-    "julia",
-    "julia-1.6.1",
-    "bin",
-)  # mpath+"/src/julia/julia-1.6.1/bin/julia" # path for julia
-os.environ["PATH"] += os.pathsep + os.path.join(julia_bin)
+julia_cmd = "julia"
 # print(os.environ["PATH"])
 jlsession = Julia(runtime=julia_cmd, compiled_modules=False, sysimage=sysimage)
 jlsession.eval("using Suppressor")  # suppress output
